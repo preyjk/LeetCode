cc2c76ae27669d9ae1a2a3b3929bbf3ac25a98fa
荆凯 <>
2024-04-25 17:46:41 +1200

    P10 sub

---
+++ src/Algorithm/P10_IsMatch.java
@@ -0,0 +1,39 @@
+package Algorithm;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Question:
+ *
+ * <p>
+ * Solution:
+ */
+public class P10_IsMatch {
+    public static void main(String[] args) {
+        // 正则表达式的基本功能: 匹配、查找、替换、分割
+        // 匹配
+        String text = "hello123";
+        String patternString = "^[a-zA-Z]+\\d+$";
+        // "." 匹配任意字符 and "*" 匹配任意次数
+        text = "%aaa";
+        patternString = ".a*";
+        Pattern pattern = Pattern.compile(patternString);
+        Matcher matcher = pattern.matcher(text);
+
+        if (matcher.matches()) {
+            System.out.println("match.");
+        } else {
+            System.out.println("not match.");
+        }
+    }
+
+    /**
+     * Method 1:
+     * 时间复杂度:
+     * 空间复杂度:
+     */
+    public boolean isMatch(String s, String p) {
+        return false;
+    }
+}
